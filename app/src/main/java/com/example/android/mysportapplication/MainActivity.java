871feae74ca6e2665adad7d4c00cd8e6a9e53e7e
gmklenevskiy <gmklenevskiy@gmail.com>
2018-05-30 15:22:14 +0300

    version 1.1

--- app/src/main/java/com/example/android/mysportapplication/MainActivity.java
+++ app/src/main/java/com/example/android/mysportapplication/MainActivity.java
@@ -4,11 +4,15 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -21,8 +25,9 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     private NewsAdapter mAdapter;
     private static final int NEWS_LOADER_ID = 1;
-    private static final  String USGS_REQUEST_URL =
-            "http://content.guardianapis.com/search?q=sport&show-references=author&show-tags=contributor&from-date=2018-01-01&show-fields=show-fields=starRating,headline,thumbnail,short-url&order-by=relevance&order-by=relevance&api-key=414e0006-1ca3-48be-b4d5-f986708eea8a";
+    private static final  String USGS_REQUEST_URL = "https://content.guardianapis.com/search";
+            //"http://content.guardianapis.com/search?section=sport&show-references=author&show-tags=contributor&from-date=2018-01-01&show-fields=show-fields=starRating,headline,thumbnail,short-url&order-by=relevance&order-by=relevance&api-key=414e0006-1ca3-48be-b4d5-f986708eea8a";
+
     private TextView mEmptyStateTextView;
 
     @Override
@@ -57,7 +62,23 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     @Override
     public Loader<List<MyNews>> onCreateLoader(int i, Bundle bundle) {
-        return new NewsLoader(this,USGS_REQUEST_URL);
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String category = sharedPrefs.getString(
+                getString(R.string.settings_category_key),
+                getString(R.string.settings_category_default));
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default));
+        Uri baseUri = Uri.parse(USGS_REQUEST_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+        uriBuilder.appendQueryParameter("section",category.toLowerCase());
+        uriBuilder.appendQueryParameter("show-tags","contributor");
+        uriBuilder.appendQueryParameter("from-date","2018-01-01");
+        uriBuilder.appendQueryParameter("show-fields","thumbnail");
+        uriBuilder.appendQueryParameter("order-by",orderBy);
+        uriBuilder.appendQueryParameter("api-key","414e0006-1ca3-48be-b4d5-f986708eea8a");
+
+        return new NewsLoader(this,uriBuilder.toString());
     }
 
     @Override
@@ -74,5 +95,22 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public void onLoaderReset(Loader<List<MyNews>> loader) {
         mAdapter.clear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
